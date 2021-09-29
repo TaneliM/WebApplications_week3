a2643f4ad1c3e467b5553b7a5a56d85700b31c01
TaneliM <64307246+TaneliM@users.noreply.github.com>
2021-09-29 14:24:52 +0300

    Added dogs

--- src/index.js
+++ src/index.js
@@ -9,5 +9,42 @@ if (document.readyState !== "loading") {
 }
 
 function initializeCode() {
-  document.getElementById("app").innerHTML = "<h1>Hello!</h1>";
+  var breeds = ["shiba", "pembroke", "samoyed", "corgi", "husky"];
+  breeds = breeds.sort(); // I'm lazy
+
+  for (var i = 0; i < breeds.length; i++) {
+    var wikiItem = document.createElement("div");
+    var wikiTitle = document.createElement("h1");
+    var wikiContent = document.createElement("div");
+    var wikiText = document.createElement("p");
+    var wikiImage = document.createElement("img");
+
+    wikiItem.className = "wiki-item";
+    wikiTitle.className = "wiki-header";
+    wikiContent.className = "wiki-content";
+    wikiText.className = "wiki-text";
+    wikiImage.className = "wiki-img";
+
+    wikiTitle.textContent = breeds[i];
+    wikiText.textContent =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ultricies nunc libero, eget suscipit urna dapibus non. Nunc cursus hendrerit ipsum, nec viverra sem dapibus id. Aliquam luctus fermentum arcu. Proin ultrices, velit eu sagittis suscipit, magna dolor malesuada sapien, non dictum diam dolor ut ligula. Quisque a neque congue, ullamcorper leo sed, ultricies nisi. Praesent gravida, purus et interdum dictum, odio ligula tincidunt ante, pretium varius nisi urna ut mauris. Sed at accumsan ligula, at sagittis est. Etiam at vestibulum sapien, vitae sollicitudin mauris. Nam non quam eleifend, tincidunt lorem sollicitudin, pharetra sapien. Etiam eleifend justo lectus, nec eleifend justo maximus sed.";
+
+    let url = "https://dog.ceo/api/breed/" + breeds[i] + "/images/random";
+
+    fetchImageUrl(url, wikiImage);
+
+    wikiContent.appendChild(wikiText);
+    wikiContent.appendChild(wikiImage);
+    wikiItem.appendChild(wikiTitle);
+    wikiItem.appendChild(wikiContent);
+
+    var container = document.getElementsByClassName("container");
+    container[0].appendChild(wikiItem);
+  }
+}
+
+async function fetchImageUrl(url, wikiImage) {
+  let response = await fetch(url);
+  let image = await response.json();
+  wikiImage.src = image.message;
 }
